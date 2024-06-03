8e1f27927a009d506bcb9f55fa6a1a92eedf3487
Muhammad Rafael Ghifari <business.rafaelghifari@gmail.com>
2024-06-03 08:49:47 +0700

    feat(map): add marker click to redirect to Google Maps
    - Added event handler for marker click
    - Clicking on the marker now redirects to 'https://maps.app.goo.gl/m4mMK6zFLT979j928'"

--- src/components/LocationMap.tsx
+++ src/components/LocationMap.tsx
@@ -25,6 +25,9 @@ const defaultIcon = L.icon({
 L.Marker.prototype.options.icon = defaultIcon;
 
 const LocationMap: React.FC = () => {
+	const handleMarkerClick = () => {
+		window.open("https://maps.app.goo.gl/m4mMK6zFLT979j928", "_blank");
+	};
 	return (
 		<MapContainer
 			center={[1.1034140062718207, 104.03177533778977]}
@@ -34,7 +37,9 @@ const LocationMap: React.FC = () => {
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 			/>
-			<Marker position={[1.1034140062718207, 104.03177533778977]}>
+			<Marker
+				position={[1.1034140062718207, 104.03177533778977]}
+				eventHandlers={{ click: handleMarkerClick }}>
 				<Popup>Radisson Golf & Convention Center Batam</Popup>
 			</Marker>
 		</MapContainer>
